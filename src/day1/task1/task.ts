b010f96015d6dced20190b96a068bedcc0e0267a
disco-trooper <1koblizek1@gmail.com>
2023-12-01 16:53:25 +0100

    feat: add part 2 solution

--- src/day1/task1/task.ts
+++ src/day1/task1/task.ts
@@ -1,9 +1,8 @@
 import { input } from "./taskInput";
 
-// Split the lines
+// Part 1 start
 const inputLines = input.split("\n");
 
-// Create array consisting of 1st and last number of each line
 const nums = inputLines.map((line) => {
   const lineNums = line.replace(/\D/g, "");
   if (lineNums.length === 1) {
@@ -13,9 +12,97 @@ const nums = inputLines.map((line) => {
   }
 });
 
-// Sum it
 const numsSummed = nums.reduce((accumulator, currentValue) => {
   return accumulator + currentValue;
 }, 0);
 
-console.log(numsSummed);
+console.log("Part 1 result", numsSummed);
+// Part 1 end
+
+// Part 2 start
+const alphaNumDict: Record<string, string> = {
+  one: "1",
+  two: "2",
+  three: "3",
+  four: "4",
+  five: "5",
+  six: "6",
+  seven: "7",
+  eight: "8",
+  nine: "9",
+};
+
+const realNums: number[] = [];
+
+inputLines.map((line) => {
+  const locations: Record<string, { first: any; last: any }> = {
+    "1": { first: null, last: null },
+    "2": { first: null, last: null },
+    "3": { first: null, last: null },
+    "4": { first: null, last: null },
+    "5": { first: null, last: null },
+    "6": { first: null, last: null },
+    "7": { first: null, last: null },
+    "8": { first: null, last: null },
+    "9": { first: null, last: null },
+    one: { first: null, last: null },
+    two: { first: null, last: null },
+    three: { first: null, last: null },
+    four: { first: null, last: null },
+    five: { first: null, last: null },
+    six: { first: null, last: null },
+    seven: { first: null, last: null },
+    eight: { first: null, last: null },
+    nine: { first: null, last: null },
+  };
+
+  const locationKeys = Object.keys(locations);
+  locationKeys.forEach((key) => {
+    locations[key]["first"] = line.indexOf(key);
+    locations[key]["last"] = line.lastIndexOf(key);
+  });
+
+  const firstAndLastValues: Record<string, any> = {
+    firstValue: null,
+    firstValueLocation: Number.POSITIVE_INFINITY,
+    lastValue: null,
+    lastValueLocation: Number.NEGATIVE_INFINITY,
+  };
+
+  Object.keys(locations).forEach((key, index, array) => {
+    const firstLocation = locations[key]["first"];
+    if (
+      firstLocation < firstAndLastValues.firstValueLocation &&
+      firstLocation > -1
+    ) {
+      firstAndLastValues.firstValueLocation = firstLocation;
+      firstAndLastValues.firstValue = alphaNumDict[key]
+        ? alphaNumDict[key]
+        : key;
+    }
+
+    const lastLocation = locations[key]["last"];
+    if (
+      lastLocation > firstAndLastValues.lastValueLocation &&
+      lastLocation > -1
+    ) {
+      firstAndLastValues.lastValueLocation = lastLocation;
+
+      firstAndLastValues.lastValue = alphaNumDict[key]
+        ? alphaNumDict[key]
+        : key;
+    }
+  });
+
+  realNums.push(
+    Number(`${firstAndLastValues.firstValue}${firstAndLastValues.lastValue}`)
+  );
+});
+
+const realNumsSummed = realNums.reduce((accumulator, currentValue) => {
+  return accumulator + currentValue;
+}, 0);
+
+console.log("Part 2 result", realNumsSummed);
+
+// Part 2 end
