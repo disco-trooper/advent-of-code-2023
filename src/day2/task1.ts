445eb622b42f430d4e1c014fa45458a8097e51b6
disco-trooper <1koblizek1@gmail.com>
2023-12-02 15:36:47 +0100

    style: remove spaces

--- src/day2/task1.ts
+++ src/day2/task1.ts
@@ -5,9 +5,7 @@ import { input } from "./taskInput";
 const MAX_RED_COUNT = 12;
 const MAX_GREEN_COUNT = 13;
 const MAX_BLUE_COUNT = 14;
-
 const games = getLines(input);
-
 const filteredGameIds = games
   .map((game) => {
     const gameId = Number(
@@ -15,7 +13,6 @@ const filteredGameIds = games
     );
     const rolls = getRolls(game);
     const maxGameValues = getMaxValues(rolls);
-
     return maxGameValues["red"] <= MAX_RED_COUNT &&
       maxGameValues["green"] <= MAX_GREEN_COUNT &&
       maxGameValues["blue"] <= MAX_BLUE_COUNT
@@ -23,7 +20,6 @@ const filteredGameIds = games
       : null;
   })
   .filter((el) => el) as number[];
-
 const sumOfIds = getSumOfValues(filteredGameIds);
 
 console.log("Task 1 result", sumOfIds);
--- src/day2/task2.ts
+++ src/day2/task2.ts
@@ -3,16 +3,13 @@ import { getLines, getSumOfValues } from "../utils";
 import { input } from "./taskInput";
 
 const games = getLines(input);
-
 const powerValues = games
   .map((game) => {
     const rolls = getRolls(game);
     const maxGameValues = getMaxValues(rolls);
-
     return maxGameValues.red * maxGameValues.green * maxGameValues.blue;
   })
   .filter((el) => el) as number[];
-
 const sumOfPowers = getSumOfValues(powerValues);
 
 console.log("Task 2 result", sumOfPowers);
